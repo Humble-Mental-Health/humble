a38c972bccbb5dba7ed421a3b18ce6e97ed6a25c
krishna-s11 <krishnasaxena69@gmail.com>
2021-05-23 15:20:35 +0530

    nav fix

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -3,19 +3,11 @@ import AboutUs from '../../Container/HomePage/AboutUs/AboutUs';
 import Banner from '../../Container/HomePage/Banner/Banner';
 import ContactUs from '../../Container/HomePage/ContactUs/ContactUs';
 import Services from '../../Container/HomePage/Services/Services';
-// import Navbar from '../../Container/Navebar/Navbar'
-import NavbarDesktop from '../../Container/NavbarDesktop/NavbarDesktop';
-//import NavbarMobile from '../../Container/NavebarMobile/NavbarMobile';
 import Psychological from '../../Container/ServicesPage/Psychological/psychological'
 import Community from '../../Container/ServicesPage/Community/Community'
 import Footer from '../../Container/Footer/Footer';
 
 function HomePage() {
-
-  
-
- 
-
   return (
     <div className="homepage">
         <Banner/>
--- src/Pages/ServicesPage/ServicesPage.js
+++ src/Pages/ServicesPage/ServicesPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import Navbar from '../../Container/Navebar/Navbar'
 // import Pricing from '../../Container/Pricing/Pricing'
 // import ServicesBanner from '../../Container/ServicesBanner/ServicesBanner'
 // import ServicesGroup from '../../Container/ServicesGroup/ServicesGroup'
@@ -11,29 +10,11 @@ import './ServicesPage.css'
 import Hero from '../../Container/ServicesPage/Hero/hero'
 import Psychological from '../../Container/ServicesPage/Psychological/psychological'
 import Yoga from '../../Container/ServicesPage/Yoga/yoga'
-import NavbarDesktop from '../../Container/NavbarDesktop/NavbarDesktop';
-import NavbarMobile from '../../Container/NavebarMobile/NavbarMobile';
 import Footer from '../../Container/Footer/Footer'
 
 function ServicesPage() {
-
-    // function detectMob() {
-    //     return ( ( window.innerWidth <= 600 ) && ( window.innerHeight <= 800 ) );
-    //   }
-    
-    //   console.log(detectMob())
-    
-    //   function navbar() {
-    //     if (detectMob())
-    //       return <NavbarMobile/>
-    //     else
-    //       return <NavbarDesktop/>
-    //   }
-
     return (
         <div className='servicesPage'>
-            {/* <Navbar isVisible='True' colour='Green'/> */}
-            {/* {navbar()} */}
             <Hero />
             <Psychological />
             <Yoga />
